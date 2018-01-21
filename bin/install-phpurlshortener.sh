a81e9fa0f70f67df5b39f8fe3d2c822660b97d95
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-01-20 20:22:50 -0600

    Fix php url shortener install

---
+++ bin/install-phpurlshortener.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf urlshortenerphp.kellenschmidt.com
+git clone https://github.com/kellenschmidt/urlshortenerphp.kellenschmidt.com.git
+cd urlshortenerphp.kellenschmidt.com
--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sh bin/install-frontend.sh $1
-sh bin/install-frontend.sh $1
+sh bin/install-backend.sh $1
+sh bin/install-phpurlshortener.sh
