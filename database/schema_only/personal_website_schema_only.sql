9a4db7a375169afee3808a8f41322efa53dd2264
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-01-25 21:57:15 -0600

    Update schema to remove deprecated table

--- database/schema_only/personal_website_schema_only.sql
+++ database/schema_only/personal_website_schema_only.sql
@@ -1,13 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 26, 2018 at 03:53 AM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `url_shortener`
+--
 CREATE DATABASE IF NOT EXISTS `personal_website` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `personal_website`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `card_content`
+--
 
 DROP TABLE IF EXISTS `card_content`;
 CREATE TABLE `card_content` (
@@ -25,6 +44,12 @@ CREATE TABLE `card_content` (
   `visible` int(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chip_content`
+--
+
 DROP TABLE IF EXISTS `chip_content`;
 CREATE TABLE `chip_content` (
   `chip_id` int(11) NOT NULL,
@@ -36,6 +61,12 @@ CREATE TABLE `chip_content` (
   `visible` int(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_content`
+--
+
 DROP TABLE IF EXISTS `course_content`;
 CREATE TABLE `course_content` (
   `course_id` int(11) NOT NULL,
@@ -44,6 +75,12 @@ CREATE TABLE `course_content` (
   `line_breaks` int(1) NOT NULL,
   `visible` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interactions`
+--
 
 DROP TABLE IF EXISTS `interactions`;
 CREATE TABLE `interactions` (
@@ -56,6 +93,12 @@ CREATE TABLE `interactions` (
   `interaction_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `links`
+--
+
 DROP TABLE IF EXISTS `links`;
 CREATE TABLE `links` (
   `code` varchar(30) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
@@ -66,14 +109,11 @@ CREATE TABLE `links` (
   `visible` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `modal_content`;
-CREATE TABLE `modal_content` (
-  `name` varchar(255) NOT NULL,
-  `header` varchar(255) NOT NULL,
-  `body` varchar(8192) NOT NULL,
-  `github_link` varchar(255) NOT NULL,
-  `accent_color` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_visits`
+--
 
 DROP TABLE IF EXISTS `page_visits`;
 CREATE TABLE `page_visits` (
@@ -85,6 +125,12 @@ CREATE TABLE `page_visits` (
   `referrer` varchar(255) NOT NULL,
   `page_visit_datetime` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -99,44 +145,82 @@ CREATE TABLE `users` (
   `verified_email` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `card_content`
+--
 ALTER TABLE `card_content`
   ADD PRIMARY KEY (`card_id`);
 
+--
+-- Indexes for table `chip_content`
+--
 ALTER TABLE `chip_content`
   ADD PRIMARY KEY (`chip_id`);
 
+--
+-- Indexes for table `course_content`
+--
 ALTER TABLE `course_content`
   ADD PRIMARY KEY (`course_id`);
 
+--
+-- Indexes for table `interactions`
+--
 ALTER TABLE `interactions`
   ADD PRIMARY KEY (`interaction_id`);
 
+--
+-- Indexes for table `links`
+--
 ALTER TABLE `links`
   ADD PRIMARY KEY (`code`);
 
-ALTER TABLE `modal_content`
-  ADD PRIMARY KEY (`name`),
-  ADD UNIQUE KEY `name` (`name`);
-
+--
+-- Indexes for table `page_visits`
+--
 ALTER TABLE `page_visits`
   ADD PRIMARY KEY (`page_visit_id`);
 
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `email` (`email`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `card_content`
+--
 ALTER TABLE `card_content`
-  MODIFY `card_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `card_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `chip_content`
+--
 ALTER TABLE `chip_content`
-  MODIFY `chip_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=306;
+  MODIFY `chip_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=308;
+--
+-- AUTO_INCREMENT for table `interactions`
+--
 ALTER TABLE `interactions`
-  MODIFY `interaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1154;
+  MODIFY `interaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1160;
+--
+-- AUTO_INCREMENT for table `page_visits`
+--
 ALTER TABLE `page_visits`
-  MODIFY `page_visit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4472;
+  MODIFY `page_visit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4571;
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
