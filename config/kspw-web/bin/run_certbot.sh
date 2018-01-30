a6cc1e116e6172b0c1b53a52bf269c88c873e9db
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-01-30 00:02:08 -0600

    Attempt at HTTPS auth with certbot, must test in prod

--- config/kspw-web/bin/run_certbot.sh
+++ config/kspw-web/bin/run_certbot.sh
@@ -5,4 +5,4 @@
 
 wget https://dl.eff.org/certbot-auto -P /usr/bin
 chmod a+x /usr/bin/certbot-auto
-certbot-auto --non-interactive --agree-tos --email kellen@kellenschmidt.com --authenticator standalone --installer apache --domains $1
+certbot-auto --non-interactive --agree-tos --email kellenschmidt31@gmail.com --authenticator webroot -w /var/www/html --installer apache --domains $1
