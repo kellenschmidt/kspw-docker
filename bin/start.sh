b4b5f4cd7c08a1b8d0907a5c7493cb90e5dda13f
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-01-31 19:41:26 -0600

    Allow acme challenge to work for CA verification

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Arguments
-# $1: Name of environment that is being deployed
+# $1: File path to Docker compose file relative to root package.json
 
 docker-compose -f $1 up -d --build --force-recreate
