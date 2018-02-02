235b74982fde55164bfe5ebab62a69107b53d7bc
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-02-01 20:17:49 -0600

    Auto remove backups older than 3 versions for every interval

--- config/kspw-db/add_cronjobs.sh
+++ config/kspw-db/add_cronjobs.sh
@@ -6,8 +6,12 @@ apt-get update -y && apt-get install -y cron nano
 # Add environment variables to Cron's environment
 printenv >> /etc/environment
 
+# Combine individual cron files into single cron file
+cat /home/cron/backup-db.cron > /home/cron/combined.cron
+cat /home/cron/remove-backups.cron >> /home/cron/combined.cron
+
 # Add Cron commands to crontab
-crontab /home/cron/backup-db.cron
+crontab /home/cron/combined.cron
 
 # Start cron
 cron
--- config/kspw-db/backup_db.sh
+++ config/kspw-db/backup_db.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Arguments
+# $1: Additional text to be added to output file name
+
 date=$(date +"%d-%m-%Y_%H:%M:%S")
-mysqldump -u $MYSQL_USER --password=$MYSQL_PASSWORD --lock-tables=false --add-drop-database --add-drop-table --databases personal_website url_shortener_php | gzip > /home/db-backups/mysql-backup_$date.sql.gz
+mysqldump -u $MYSQL_USER --password=$MYSQL_PASSWORD --lock-tables=false --add-drop-database --add-drop-table --databases personal_website url_shortener_php | gzip > /home/db-backups/mysql-$1-backup_$date.sql.gz
