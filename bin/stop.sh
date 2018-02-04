f1b6fe660700f643a65005d2618d6efa1320247a
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-02-04 16:11:05 -0600

    Remove old backups and upload to cloud when container stopped

--- bin/stop.sh
+++ bin/stop.sh
@@ -4,6 +4,8 @@
 # $1: Name of environment that is being deployed
 
 docker exec kspw-db bash /home/bin/backup_db.sh exit
+docker exec kspw-db bash /home/bin/remove_backups.sh exit
+docker exec kspw-db bash /home/bin/upload_cloud.sh
 
 docker-compose -f $1 stop
 docker rm kspw-web \
--- config/kspw-db/bin/upload_cloud.sh
+++ config/kspw-db/bin/upload_cloud.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Arguments
-# 
+# None
 
 newestBackup=$(ls -t /home/db-backups | head -1)
-echo "Newest backup: ${newestBackup}" >> /var/log/cron.log
-dropbox_uploader.sh -f /etc/environment upload /home/db-backups/${newestBackup} ./
-# dropbox_uploader.sh -f /etc/environment upload "$(ls -t /home/db-backups | head -1)" ./
 
+dropbox_uploader.sh -f /etc/environment upload /home/db-backups/${newestBackup} ./ >> /var/log/cron.log
