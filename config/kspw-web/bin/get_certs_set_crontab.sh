21bd84bf2f9237e39625cefcc31ae9fd75ff6f80
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-02-18 15:42:02 -0600

    Get certs for all domains regardless of environment

--- config/kspw-web/bin/get_certs_set_crontab.sh
+++ config/kspw-web/bin/get_certs_set_crontab.sh
@@ -2,14 +2,9 @@
 
 # Arguments
 # None
-echo "ENV: $ENV" >> /var/log/test.log
 
-if [ "$ENV" = "prod" ]
-then
-    bash /home/bin/run_certbot.sh kellenschmidt.com,api.kellenschmidt.com,urlshortenerphp.kellenschmidt.com
-    crontab /etc/cron.d/renew-certs.cron
-elif [ "$ENV" = "test" ]
-then
-    bash /home/bin/run_certbot.sh kellenschmidt.com,api.kellenschmidt.com,urlshortenerphp.kellenschmidt.com,test.kellenschmidt.com,testapi.kellenschmidt.com
-    crontab /etc/cron.d/renew-certs.cron
-fi
+wget https://dl.eff.org/certbot-auto -P /usr/bin
+chmod a+x /usr/bin/certbot-auto
+certbot-auto --non-interactive --agree-tos --email kellenschmidt31@gmail.com --authenticator webroot -w /var/www/html --installer apache --domains kellenschmidt.com,api.kellenschmidt.com,urlshortenerphp.kellenschmidt.com,test.kellenschmidt.com,testapi.kellenschmidt.com
+
+crontab /etc/cron.d/renew-certs.cron
--- config/kspw-web/bin/run_certbot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Arguments
-# $1: Comma (no spaces) seperated list of URLs to get HTTPS certificates for
-
-wget https://dl.eff.org/certbot-auto -P /usr/bin
-chmod a+x /usr/bin/certbot-auto
-certbot-auto --non-interactive --agree-tos --email kellenschmidt31@gmail.com --authenticator webroot -w /var/www/html --installer apache --domains $1
