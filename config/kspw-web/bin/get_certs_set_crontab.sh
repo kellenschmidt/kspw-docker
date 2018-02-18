721d4495a144c4b8b7edd07c493f31e9307b55e0
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-02-18 16:21:59 -0600

    Change certbot to use apache plugin for certs

--- config/kspw-web/bin/get_certs_set_crontab.sh
+++ config/kspw-web/bin/get_certs_set_crontab.sh
@@ -5,6 +5,6 @@
 
 wget https://dl.eff.org/certbot-auto -P /usr/bin
 chmod a+x /usr/bin/certbot-auto
-certbot-auto --non-interactive --agree-tos --email kellenschmidt31@gmail.com --authenticator webroot -w /var/www/html --installer apache --domains kellenschmidt.com,api.kellenschmidt.com,urlshortenerphp.kellenschmidt.com,test.kellenschmidt.com,testapi.kellenschmidt.com
+certbot-auto --non-interactive --agree-tos --apache --email kellenschmidt31@gmail.com --domains kellenschmidt.com,api.kellenschmidt.com,urlshortenerphp.kellenschmidt.com,test.kellenschmidt.com,testapi.kellenschmidt.com > /var/log/certs.log
 
 crontab /etc/cron.d/renew-certs.cron
