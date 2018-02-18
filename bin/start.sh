2bbb30474d6e22e62ab23e77e684657982fc60a1
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-02-18 13:13:51 -0600

    Attempt to stop using cache on builds at all costs

--- bin/start.sh
+++ bin/start.sh
@@ -7,7 +7,8 @@
 bash bin/restore_backup.sh $1
 
 # Start Docker
-docker-compose -f config/docker-compose.yml up -d --build --force-recreate
+docker-compose --no-cache -f config/docker-compose.yml build
+docker-compose --build --force-recreate -f config/docker-compose.yml up
 
 # Start cron inside Docker
 docker exec kspw-db cron
--- config/kspw-web/bin/get_certs_set_crontab.sh
+++ config/kspw-web/bin/get_certs_set_crontab.sh
@@ -2,6 +2,7 @@
 
 # Arguments
 # None
+echo "ENV: $ENV" >> /var/log/test.log
 
 if [ "$ENV" = "prod" ]
 then
@@ -9,6 +10,6 @@ then
     crontab /etc/cron.d/renew-certs.cron
 elif [ "$ENV" = "test" ]
 then
-    bash /home/bin/run_certbot.sh test.kellenschmidt.com,testapi.kellenschmidt.com;
+    bash /home/bin/run_certbot.sh kellenschmidt.com,api.kellenschmidt.com,urlshortenerphp.kellenschmidt.com,test.kellenschmidt.com,testapi.kellenschmidt.com
     crontab /etc/cron.d/renew-certs.cron
 fi
