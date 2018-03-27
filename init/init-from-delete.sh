e65fa03b15190019f203371f871e20e77a78b84b
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-03-27 17:19:45 -0500

    Add scripts for handling deleting and redownload of docker code

---
+++ init/init-from-delete.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Arguments
+# None
+
+if [ ! -d ~/init ]; then
+  cp ~/kspw-docker/init ~/init
+  echo "Copying init folder from kspw-docker/ to ~/"
+fi
+
+backupFile="~/init/mysql-prod-*_*.sql.gz"
+newestBackup=$(ls -t ${backupFile} | head -1)
+
+if [ -f "$newestBackup" ]; then
+  # Restore the newest backup
+  cp ${newestBackup} ~/kspw-docker/config/kspw-cron/db-backups
+  echo "Copying newest backup (${newestBackup}) from init/ into kspw-docker/"
+elif [ -f "*.sql"]; then
+  # Restore from sql file(s)
+  cp *.sql ~/kspw-docker/config/kspw-db/schema-with-data
+  echo "Copying database sql files from init/ into kspw-docker/"
+else
+  echo "No init files provided"
+fi
