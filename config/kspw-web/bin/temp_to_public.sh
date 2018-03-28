8f7f572bdd2c3e96db9063cb7b6f213c5655b19d
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-03-27 21:35:31 -0500

    Create and remove public directory before and after doing https cert

--- config/kspw-web/bin/temp_to_public.sh
+++ config/kspw-web/bin/temp_to_public.sh
@@ -3,8 +3,14 @@
 # Arguments
 # None
 
-mv /var/www/kellenschmidt.com/public_temp/* /var/www/kellenschmidt.com/public/
-mv /var/www/api.kellenschmidt.com/public_temp/* /var/www/api.kellenschmidt.com/public/
-mv /var/www/urlshortenerphp.kellenschmidt.com/public_temp/* /var/www/urlshortenerphp.kellenschmidt.com/public/
-mv /var/www/test.kellenschmidt.com/public_temp/* /var/www/test.kellenschmidt.com/public/
-mv /var/www/testapi.kellenschmidt.com/public_temp/* /var/www/testapi.kellenschmidt.com/public/
+rm -rf /var/www/kellenschmidt.com/public \\
+       /var/www/api.kellenschmidt.com/public \\
+       /var/www/urlshortenerphp.kellenschmidt.com/public \\
+       /var/www/test.kellenschmidt.com/public \\
+       /var/www/testapi.kellenschmidt.com/public
+
+mv /var/www/kellenschmidt.com/public_temp /var/www/kellenschmidt.com/public
+mv /var/www/api.kellenschmidt.com/public_temp /var/www/api.kellenschmidt.com/public
+mv /var/www/urlshortenerphp.kellenschmidt.com/public_temp /var/www/urlshortenerphp.kellenschmidt.com/public
+mv /var/www/test.kellenschmidt.com/public_temp /var/www/test.kellenschmidt.com/public
+mv /var/www/testapi.kellenschmidt.com/public_temp /var/www/testapi.kellenschmidt.com/public
