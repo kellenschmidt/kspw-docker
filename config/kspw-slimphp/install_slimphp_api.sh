bc792e73ea9f92eedd2d3f638084a3a7a6599f83
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-05-22 00:37:49 -0600

    Running at webserver root

---
+++ config/kspw-slimphp/install_slimphp_api.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Arguments
+# $1: Name of environment that is being deployed
+
+git clone https://github.com/kellenschmidt/api.kellenschmidt.com.git /home/api.kellenschmidt.com
+cd /home/api.kellenschmidt.com
+
+if [ "$1" == "dev" ]; then
+	git checkout -b dev origin/dev
+elif [ "$1" == "test" ]; then
+	git checkout -b staging origin/staging
+fi
+
+sh install_composer.sh
+php composer.phar install
+
+mv /home/api.kellenschmidt.com/* /var/www
+mv /var/www/public/* /var/www/html
+rm -r /home/api.kellenschmidt.com /var/www/public
