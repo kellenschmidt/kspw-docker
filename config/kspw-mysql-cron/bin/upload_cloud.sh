22010d588450b6a2146564e105090a5412b932d6
Kellen Schmidt <kellenschmidt31@gmail.com>
2018-05-24 17:40:47 -0600

    Sort uploads into folders by environment

--- config/kspw-mysql-cron/bin/upload_cloud.sh
+++ config/kspw-mysql-cron/bin/upload_cloud.sh
@@ -6,4 +6,4 @@
 newestBackup=$(ls -t /home/backups | head -1)
 
 echo $OAUTH_ACCESS_TOKEN > ~/.dropbox_uploader
-dropbox_uploader.sh upload /home/backups/${newestBackup} /mysql/${newestBackup}
+dropbox_uploader.sh upload /home/backups/${newestBackup} /mysql/$ENV/${newestBackup}
